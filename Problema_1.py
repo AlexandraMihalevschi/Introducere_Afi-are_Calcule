f0efd907fa408402b264070349a9e08d2c1cd011
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2020-10-05 19:18:59 +0300

    Add files via upload

---
+++ Problema_1.py
@@ -0,0 +1,16 @@
+a = int(input("Dă numărul de ordine al culorii "))
+if a==1:
+    print("Roșu")
+elif a==2:
+    print("Oranj")
+elif a==3:
+    print("Galben")
+elif a==4:
+    print("Verde")
+elif a==5:
+    print("Albastru")
+elif a==6:
+    print("Indigo")
+elif a==7:
+    print("Violet")
+else: print("Invata culorile")
