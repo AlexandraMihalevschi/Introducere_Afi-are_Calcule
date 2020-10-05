157878557a77cf10a26650ef343e1760b8c3ec43
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2020-10-05 19:55:12 +0300

    Add files via upload

---
+++ Problema_7.py
@@ -0,0 +1,5 @@
+varsta = int(input("Dati varsta copilului: "))
+greutate = (2*varsta)+8
+inaltime = (5*varsta)+80
+print("Greutatea ideala este de", greutate, "kg")
+print("Inaltimea ideala este de", inaltime, "cm")
