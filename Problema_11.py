3f00fe8a8488581ca909d534ed2a21da86152099
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2020-10-05 21:14:24 +0300

    Add files via upload

---
+++ Problema_11.py
@@ -0,0 +1,4 @@
+a = int(input("Dati numarul de iepuri la inc. lunii: "))
+b = int(input("Dati numarul de iepuri morti: "))
+c = int(input("Dati numarul de iepuri nascuti: "))
+print("Numarul total de iepuri:", a-b+c)
